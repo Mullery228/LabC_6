bc2fb6222b6a9fced0f7851e6159bb6381c23ec3
Mullery228 <102748044+Mullery228@users.noreply.github.com>
2023-03-29 22:55:09 +0700

    Add files via upload

---
+++ Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿void Programm(int m)
+{
+    int count = 0;
+    int[] array = new int[m];
+    for(int i = 0; i < array.Length; i++)
+    {
+        int a = Convert.ToInt32(Console.ReadLine());
+        array[i] = a;
+    }
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] >= 0)
+        {
+            count++;
+        } 
+    }
+    Console.Write($"Количество неотрицательных чисел: {count}");
+}
+
+Console.WriteLine("Введите кол-во чисел: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Programm(M);
---
+++ Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿void Programm(double k1, double k2, double b1, double b2)
+{
+    double x = -(b1 - b2) / (k1 - k2);
+    double y = k1 * x + b1;
+    Console.WriteLine($"Точка пересечения: ({x};{y})");
+}
+
+Console.WriteLine("Введите параметр k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите параметр k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите параметр b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите параметр b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Programm(k1, k2, b1, b2);
